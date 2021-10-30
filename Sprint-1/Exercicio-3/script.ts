f212eae1aa7b01dc7fae47aa154b97ebc0433d86
Samuel Camilo da Costa <75193747+samuelcamilodacosta@users.noreply.github.com>
2021-10-30 15:51:17 -0300

    Add files via upload

---
+++ Sprint-1/Exercicio-3/script.ts
@@ -0,0 +1,19 @@
+let list: any[] = [9, '8', '82', 321, 'asfdaf', 'abobora', '98a', '32'];
+
+let biggerValue: number = Number.MIN_VALUE,
+  smallerValue: number = Number.MAX_VALUE,
+  averageValue: number = 0,
+  cont: number = 0;
+for (let i: number = 0; i < list.length; i++) {
+  if (!isNaN(list[i])) {
+    list[i] = parseInt(list[i]);
+    (biggerValue < list[i]) ? biggerValue = list[i] : biggerValue;
+    (smallerValue > list[i]) ? smallerValue = list[i] : smallerValue;
+    averageValue += list[i];
+    cont++;
+  }
+}
+averageValue /= cont;
+let array = [biggerValue, smallerValue, averageValue];
+console.log(array);
+
