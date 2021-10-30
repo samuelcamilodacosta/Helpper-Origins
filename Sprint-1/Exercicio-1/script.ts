3915d7b70cc207ede77f4c35f62eca0cf9a23549
Samuel Camilo da Costa <75193747+samuelcamilodacosta@users.noreply.github.com>
2021-10-30 00:32:41 -0300

    Add files via upload

---
+++ Sprint-1/Exercicio-1/script.ts
@@ -0,0 +1,16 @@
+function amountVowels(word: string): number {
+  const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
+  word = word.toLowerCase();
+  word = word.normalize('NFD');
+  let result: string[] = word.split('').filter((letter: string) => vowels.includes(letter));
+  return result.length;
+}
+
+let example: string = "bootcamp origin helpper";
+console.log(amountVowels(example)); //solution (a)
+const btn = document.querySelector('#send') as HTMLInputElement;
+btn.addEventListener('click', function (e: MouseEvent) {
+  e.preventDefault();
+  let word = document.querySelector('#word') as HTMLInputElement;
+  console.log(amountVowels(word.value)); //solution (b)
+});
