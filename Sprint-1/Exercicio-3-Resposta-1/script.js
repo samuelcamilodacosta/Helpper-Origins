3160e38961522dca6bb66eaf12bcc6622de74e0b
Samuel Camilo da Costa <75193747+samuelcamilodacosta@users.noreply.github.com>
2021-11-01 12:07:03 -0300

    Add files via upload

---
+++ Sprint-1/Exercicio-3-Resposta-1/script.js
@@ -0,0 +1,26 @@
+"use strict";
+const text = prompt("Enter list: ");
+let list = text.split(", ");
+let biggerValue = 0, smallerValue = 0, averageValue = 0, validador = 0, value, index;
+for (index = 0; index < list.length; index++) {
+    if (parseFloat(list[index])) {
+        value = parseFloat(list[index]);
+        if (index == 0) {
+            smallerValue = value;
+            biggerValue = value;
+        }
+        (biggerValue < value) ? biggerValue = value : biggerValue;
+        (smallerValue > value) ? smallerValue = value : smallerValue;
+        averageValue += value;
+    }
+    else {
+        alert("This list is not numerical");
+        validador = -1;
+        break;
+    }
+}
+if (validador != -1) {
+    averageValue /= list.length;
+    let array = [biggerValue, smallerValue, averageValue];
+    alert(`The bigger value on the list: ${array[0]}.\nThe smaller value on the list: ${array[1]}.\nThe average value of the  list: ${array[2]}.`);
+}
