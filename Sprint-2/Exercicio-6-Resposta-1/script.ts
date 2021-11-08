f395d55a908a58b6080082a2c86637acc0cdcbe8
samuelcamilodacosta <samuka.camilo@gmail.com>
2021-11-08 19:46:02 -0300

    exercicio-6 imperativo e funcional

---
+++ Sprint-2/Exercicio-6-Resposta-1/script.ts
@@ -0,0 +1,23 @@
+import List from "./class.js";
+
+let list = new List([
+  {"id" : 1, "name": "Ada Lovelace", "bio" : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
+  {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"},
+  {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
+  {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}
+]);
+
+let id: number = parseInt(prompt("Enter ID to find the name: ") as string);
+list.returnName(id);
+
+id = parseInt(prompt("Enter ID to find the biography: ") as string);
+list.returnBio(id);
+
+id = parseInt(prompt("Enter ID to delete the line: ") as string);
+list.deleteLine(id);
+
+id = parseInt(prompt("Enter the ID you want to change: ") as string);
+let action: string = prompt("What you want to change, \"name\" or \"biography\"? ") as string;
+let text: string = prompt(`What you want to write on ${action}?`) as string;
+list.changeBioOrName(id, action, text);
+
