21f11e33a1f26cb1ae2f64320221d85e6ffe9227
samuelcamilodacosta <samuka.camilo@gmail.com>
2021-11-11 18:41:39 -0300

    feature/Exercicio-5

--- Sprint-2/Exercicio-5/script.ts
+++ Sprint-2/Exercicio-5/script.ts
@@ -8,18 +8,8 @@ let answerExample: HTMLElement = document.getElementById('answerExample') as HTM
 answerExample.innerHTML = `Quantidade de vogais: ${classOfExample.amountVowels()}`;
 
 // solution (b)
-/**
-  * User event interation 
-  * When the event button is clicked, it validates to prevent an empty value from being passed
-  * 
-  * @param word - receive the value of the input id #word
-  * @param classOfWord - creates a class with the constructor method passing the word string value
-  * 
-  * @returns the result of applying the amount vowels method if string is not empty else returns an alert.
-  */
 const button: HTMLButtonElement = document.querySelector('#send') as HTMLButtonElement;
 button.addEventListener('click', actionOnClick);
-
 /**
  * This function is responsible for validating the input and, if possible, 
  * printing the response
