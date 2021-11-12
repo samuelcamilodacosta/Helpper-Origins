321f6e4360dd107d93ddac17b5fe533f86d81865
samuelcamilodacosta <samuka.camilo@gmail.com>
2021-11-11 23:39:51 -0300

    feature/Exercicio-7

--- Sprint-2/Exercicio-7-Resposta-1/script.ts
+++ Sprint-2/Exercicio-7-Resposta-1/script.ts
@@ -3,14 +3,10 @@ const text: string = prompt("Enter list: ") as string;
 let list: List = new List(text.split(", "));
 let answer: number[];
 
+//Checks if the list is numeric with method numericalList
 if(list.numericalList() != -1){
-  /**
-   * Initially checks if the list is numeric with method numericalList.
-   * 
-   * If so, displays the values returned from a method applied to the list variable.
-   * 
-   * Otherwise it displays a warning saying the list is not numeric.
-   */
+
+  //Applies a method on the variable list that returns a numeric array of three values to answer.
   answer = list.findBiggerSmallerAndAverageValue();
   alert (`The bigger value on the list: ${answer[0]}.\nThe smaller value on the list: ${answer[1]}.\nThe average value of the  list: ${answer[2]}.`);
 }else{
